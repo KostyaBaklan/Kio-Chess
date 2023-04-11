05b9dc3da8fadb61f9b01ca65bef189ed3296c70
Kostya Baklan <kostya_baklan@yahoo.com>
2023-04-11 11:08:18 +0300

    Update config

--- KioChess/Engine/Models/Enums/Phase.cs
+++ KioChess/Engine/Models/Enums/Phase.cs
@@ -3,7 +3,7 @@
     public static class  Phase
     {
         public static byte Opening = 0;
-        public static byte Middle = 0;
-        public static byte End = 0;
+        public static byte Middle = 1;
+        public static byte End = 2;
     }
 }

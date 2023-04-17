a7affb2ede856cea043bde33a1e8c6e64236ca4f
Kostya Baklan <kostya_baklan@yahoo.com>
2023-04-17 13:49:08 +0300

    Improve promotion extension

--- KioChess/Engine/Strategies/Base/StrategyBase.cs
+++ KioChess/Engine/Strategies/Base/StrategyBase.cs
@@ -721,7 +721,7 @@ namespace Engine.Strategies.Base
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual int GetExtension(MoveBase move)
         {
-            return move.IsCheck ? 1 : 0;
+            return move.IsCheck || move.IsPromotionExtension ? 1 : 0;
         }
 
         private void InitializeMargins()

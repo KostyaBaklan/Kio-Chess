9df29c4e99d13657e835a6e0c299235c1177a591
Kostya Baklan <kostya_baklan@yahoo.com>
2023-05-31 15:54:09 +0300

    Added sort configuration

--- KioChess/Engine/Interfaces/Config/SortingConfiguration.cs
+++ KioChess/Engine/Interfaces/Config/SortingConfiguration.cs
@@ -2,11 +2,10 @@
 {
     public class SortingConfiguration
     {
-        public int[] SortDepth { get; set; }
+        public int[][] SortDepth { get; set; }
         public SortType SortType { get; set; }
         public int SortMinimum { get; set; }
         public int SortHalfIndex { get; set; }
         public int SortMoveIndex { get; set; }
-        public bool UseComplexSort { get; set; }
     }
 }

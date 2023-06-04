cacacae40e3aff54a96c62ebc423f90085c93f6f
Kostya Baklan <kostya_baklan@yahoo.com>
2023-06-04 10:20:01 +0300

    Add Bit list

--- KioChess/Engine/Models/Helpers/BitBoardExtensions.cs
+++ KioChess/Engine/Models/Helpers/BitBoardExtensions.cs
@@ -34,6 +34,12 @@ namespace Engine.Models.Helpers
             _bitService.GetPositions(b, positionsList);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void GetPositions(this BitBoard b, ref BitList positionsList)
+        {
+            _bitService.GetPositions(b, ref positionsList);
+        }
+
         //[MethodImpl(MethodImplOptions.AggressiveInlining)]
         //public static byte BitScanReverse(this BitBoard b)
         //{

62ed7dca221c3f41fe83f11ea0145ac583637114
KostyaBaklan <konstantin.baklan@toluna.com>
2023-08-22 11:02:30 +0300

    Added game tools

---
+++ KioChess/GameTool/MoveSequence.cs
@@ -0,0 +1,6 @@
+﻿public class MoveSequence
+{
+    public short M1 { get; set; }
+    public short M2 { get; set; }
+    public short M3 { get; set; }
+}
---
+++ KioChess/GameTool/Program.cs
@@ -0,0 +1,138 @@
+﻿using Engine.Book.Interfaces;
+using Engine.Book.Models;
+using Engine.DataStructures;
+using Engine.Interfaces;
+using Engine.Models.Boards;
+using Engine.Strategies.Aspiration;
+using Engine.Strategies.Base;
+using Engine.Strategies.ID;
+using Engine.Strategies.Lmr;
+using Engine.Strategies.Null;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using Tools.Common;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var timer = Stopwatch.StartNew();
+
+        Boot.SetUp();
+
+        IDataAccessService dataAccessService = Boot.GetService<IDataAccessService>();
+
+        dataAccessService.Connect();
+
+        var task = dataAccessService.LoadAsync(Boot.GetService<IBookService>());
+
+        var strategies = new List<string> { "lmrd", "lmr", "lmr_asp", "lmrd_asp", "ab_null", "lmr_null", "lmrd_null", "lmrd",
+            "id", "lmr_asp", "lmrd_asp", "lmr_null", "id", "lmrd_null", "lmrd", "lmr_asp", "lmrd_asp", "id",
+            "lmrd_null", "lmrd", "lmr_asp", "lmrd_asp","lmr_null","lmrd", "lmr", "lmr_asp", "lmrd_asp", "id", "id" };
+
+        var depths = new List<short> { 3, 4, 5, 6, 7, 8, 4, 5, 6, 7, 8, 9, 5, 6, 7, 8, 6, 7, 8, 9, 5, 6, 7, 8, 10, 3, 4, 5, 6 };
+
+        Dictionary<string, Func<short, IPosition, StrategyBase>> strategyFactories =
+                new Dictionary<string, Func<short, IPosition, StrategyBase>>
+                {
+                    {"lmr", (d, p) => new LmrStrategy(d, p)},
+                    {"lmrd", (d, p) => new LmrDeepStrategy(d, p)},
+                    {"lmr_asp", (d, p) => new LmrAspirationStrategy(d, p)},
+                    {"lmrd_asp", (d, p) => new LmrDeepAspirationStrategy(d, p)},
+                    {"id", (d, p) => new IteretiveDeepingStrategy(d, p)},
+                    {"ab_null", (d, p) => new NullNegaMaxMemoryStrategy(d, p)},
+                    {"lmr_null", (d, p) => new NullLmrStrategy(d, p)},
+                    {"lmrd_null", (d, p) => new NullLmrDeepStrategy(d, p)}
+                };
+
+        var position = new Position();
+
+        var moveProvider = Boot.GetService<IMoveProvider>();
+
+        StrategyBase whiteStrategy = strategyFactories[strategies.GetRandomItem()](depths.GetRandomItem(), position);
+
+        StrategyBase blackStrategy = strategyFactories[strategies.GetRandomItem()](depths.GetRandomItem(), position);
+
+        List<MoveSequence> list = JsonConvert.DeserializeObject<List<MoveSequence>>(File.ReadAllText("Config\\Seuquence.json"));
+
+        MoveSequence ms = list.GetRandomItem();
+
+        position.MakeFirst(moveProvider.Get(ms.M1));
+        position.Make(moveProvider.Get(ms.M2));
+        position.Make(moveProvider.Get(ms.M3));
+
+        GameResult gameResult = GameResult.Continue;
+
+        StrategyBase strategy = whiteStrategy;
+
+        try
+        {
+            while (gameResult == GameResult.Continue)
+            {
+                if (strategy == whiteStrategy)
+                {
+                    strategy = blackStrategy;
+                }
+                else
+                {
+                    strategy = whiteStrategy;
+                }
+
+                task.Wait();
+
+                dataAccessService.Disconnect();
+
+                var result = strategy.GetResult();
+
+                gameResult = result.GameResult;
+
+                if (gameResult == GameResult.Continue)
+                {
+                    position.Make(result.Move);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            var error = ex.ToFormattedString();
+
+            Console.WriteLine(error);
+
+            throw new ApplicationException("Crash", ex);
+        }
+
+        try
+        {
+            dataAccessService.Connect();
+
+            var history = position.GetHistory();
+
+            timer.Stop();
+
+            string generalMessage = $"Time = {timer.Elapsed}. [{gameResult}]. Moves = {Boot.GetService<IMoveHistoryService>().GetPly()}";
+
+            if (gameResult == GameResult.Mate)
+            {
+                if (strategy == whiteStrategy)
+                {
+                    Console.WriteLine($"Black Win -> {whiteStrategy} < {blackStrategy}. {generalMessage}");
+                    dataAccessService.AddHistory(history, GameValue.BlackWin);
+                }
+                else
+                {
+                    Console.WriteLine($"White Win -> {whiteStrategy} > {blackStrategy}. {generalMessage}");
+                    dataAccessService.AddHistory(history, GameValue.WhiteWin);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Draw -> {whiteStrategy} = {blackStrategy}. {generalMessage}");
+                dataAccessService.AddHistory(history, GameValue.Draw);
+            }
+        }
+        finally
+        {
+            dataAccessService.Disconnect();
+        }
+    }
+}

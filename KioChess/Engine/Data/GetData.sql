893100bb051e4f4c54bf2adae9c7bbdc0582e27a
KostyaBaklan <konstantin.baklan@toluna.com>
2023-08-27 07:59:55 +0300

    17 GD 10

--- KioChess/Engine/Data/GetData.sql
+++ KioChess/Engine/Data/GetData.sql
@@ -5,6 +5,25 @@ SELECT [History]
       ,[Black]
   FROM [ChessData].[dbo].[Books]
 
+  SELECT DISTINCT [History]
+  FROM [ChessData].[dbo].[Books]
+
+  SELECT [White],[Draw],[Black]
+  FROM [ChessData].[dbo].[Books]
+   WHERE [History] = ''
+
+  SELECT [White]+[Draw]+[Black]
+  FROM [ChessData].[dbo].[Books]
+  WHERE [History] = ''
+
+  SELECT SUM([White]+[Draw]+[Black])
+  FROM [ChessData].[dbo].[Books]
+  WHERE [History] = ''
+
+  SELECT p.[Key]+' '+'['+s1.[Name]+','+s2.[Name]+']' as Move, b.[White] , b.[Draw] , b.[Black],[White]+[Draw]+[Black]
+  FROM [dbo].[Books] as b, [dbo].[Moves] m,[dbo].[Pieces] p,[dbo].[Squares] s1,[dbo].[Squares] s2
+  WHERE b.History = '' and b.NextMove = m.[ID] and m.Piece = p.[Piece] and m.[From] = s1.ID and m.[To] = s2.ID
+
   SELECT [History]
       ,[NextMove]
       ,[White]

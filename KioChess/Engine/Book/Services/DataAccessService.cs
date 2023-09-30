61bde6fd6a1fabf5c5a4db1cc5802a2d468c097c
KostyaBaklan <konstantin.baklan@toluna.com>
2023-09-30 14:51:32 +0300

    Use load query with length parameter

--- KioChess/Engine/Book/Services/DataAccessService.cs
+++ KioChess/Engine/Book/Services/DataAccessService.cs
@@ -15,6 +15,7 @@ namespace Engine.Book.Services
     public class DataAccessService : IDataAccessService
     {
         private readonly int _depth;
+        private readonly int _search;
         private readonly int _threshold;
         private readonly short _games;
         private readonly SqlConnection _db;
@@ -29,6 +30,7 @@ namespace Engine.Book.Services
             IMoveHistoryService moveHistory)
         {
             _depth = configurationProvider.BookConfiguration.SaveDepth;
+            _search = configurationProvider.BookConfiguration.SearchDepth;
             _threshold = configurationProvider.BookConfiguration.SuggestedThreshold;
             _games = configurationProvider.BookConfiguration.GamesThreshold;
             var hostname = Dns.GetHostName();
@@ -144,17 +146,18 @@ namespace Engine.Book.Services
                                 (
 	                                SELECT ISNULL(B1.[History],B2.[History]) AS History, ISNULL(B1.[NextMove],B2.[NextMove]) AS NextMove,
 		                                CASE
-			                                WHEN B1.[History] IS NOT NULL AND B2.[History] IS NOT NULL THEN (B1.[White]+B1.[Draw]+B1.[Black]+B2.[White]+B2.[Draw]+B2.[Black])
-			                                WHEN B1.[History] IS NULL THEN (B2.[White]+B2.[Draw]+B2.[Black])
-			                                ELSE (B1.[White]+B1.[Draw]+B1.[Black])
+			                                WHEN B1.[History] IS NOT NULL AND B2.[History] IS NOT NULL THEN (B1.[Total]+B2.[Total])
+			                                WHEN B1.[History] IS NULL THEN (B2.[Total])
+			                                ELSE (B1.[Total])
 		                                END AS Total
-	                                FROM [ChessDB-1].[dbo].[Books] B1 FULL OUTER JOIN [ChessDB-2].[dbo].[Books] B2 ON B1.[History] = B2.[History] AND B1.[NextMove] = B2.[NextMove]
+	                                FROM [ChessDB-1].[dbo].[GameTotalHistory] (@MaxSequence) AS B1 FULL OUTER JOIN [ChessDB-2].[dbo].[GameTotalHistory] (@MaxSequence) AS B2 ON B1.[History] = B2.[History] AND B1.[NextMove] = B2.[NextMove]
                                 ) AS BOOKJOIN
                                 WHERE Total > @Games";
 
                 SqlCommand command = new SqlCommand(query, _db);
                 
                 command.Parameters.AddWithValue("@Games", _games);
+                command.Parameters.AddWithValue("@MaxSequence", 2*_search +1);
 
                 List<HistoryTotalItem> list = new List<HistoryTotalItem>();
                 List<HistoryTotalItem> open = new List<HistoryTotalItem>();
--- KioChess/Engine/Data/GetUnionData.sql
+++ KioChess/Engine/Data/GetUnionData.sql
@@ -17,3 +17,72 @@ FROM
 WHERE Total > 8
 ORDER BY LEN(History)
 
+select History, NextMove, [White]+[Draw]+[Black] AS Total
+from [ChessDB-1].[dbo].[Books]
+
+select History, NextMove, [White]+[Draw]+[Black] AS Total
+from [ChessDB-2].[dbo].[Books]
+
+
+SELECT * FROM [ChessDB-1].[dbo].[GameTotalHistory] (49)
+SELECT * FROM [ChessDB-2].[dbo].[GameTotalHistory] (49)
+SELECT * FROM [ChessDB-2].[dbo].[GameTotalHistory] (41)
+
+SELECT *
+FROM 
+(
+	SELECT ISNULL(B1.[History],B2.[History]) AS History, ISNULL(B1.[NextMove],B2.[NextMove]) AS NextMove,
+		CASE
+			WHEN B1.[History] IS NOT NULL AND B2.[History] IS NOT NULL THEN (B1.[Total]+B2.[Total])
+			WHEN B1.[History] IS NULL THEN (B2.[Total])
+			ELSE (B1.[Total])
+		END AS Total
+	FROM [ChessDB-1].[dbo].[GameTotalHistory] (41) AS B1 FULL OUTER JOIN [ChessDB-2].[dbo].[GameTotalHistory] (41) AS B2 ON B1.[History] = B2.[History] AND B1.[NextMove] = B2.[NextMove]
+) AS BOOKJOIN
+WHERE Total > 8
+
+SELECT *
+FROM 
+(
+	SELECT ISNULL(B1.[History],B2.[History]) AS History, ISNULL(B1.[NextMove],B2.[NextMove]) AS NextMove,
+		CASE
+			WHEN B1.[History] IS NOT NULL AND B2.[History] IS NOT NULL THEN (B1.[Total]+B2.[Total])
+			WHEN B1.[History] IS NULL THEN (B2.[Total])
+			ELSE (B1.[Total])
+		END AS Total
+	FROM [ChessDB-1].[dbo].[GameTotalHistory] (49) AS B1 FULL OUTER JOIN [ChessDB-2].[dbo].[GameTotalHistory] (49) AS B2 ON B1.[History] = B2.[History] AND B1.[NextMove] = B2.[NextMove]
+) AS BOOKJOIN
+WHERE Total > 8
+
+select History,NextMove, SUM(Total) AS TotalGames
+from
+	(
+		select * from [ChessDB-1].[dbo].[GameTotalHistory](49)
+		union all
+		select * from [ChessDB-2].[dbo].[GameTotalHistory](49)
+	) AS UNIONALL
+Group by History,NextMove
+having SUM(Total) > 8
+
+SELECT *
+FROM 
+(
+	SELECT ISNULL(B1.[History],B2.[History]) AS History, ISNULL(B1.[NextMove],B2.[NextMove]) AS NextMove,
+		CASE
+			WHEN B1.[History] IS NOT NULL AND B2.[History] IS NOT NULL THEN (B1.[Total]+B2.[Total])
+			WHEN B1.[History] IS NULL THEN (B2.[Total])
+			ELSE (B1.[Total])
+		END AS Total
+	FROM [ChessDB-1].[dbo].[GameTotalHistoryByLength] (49) AS B1 FULL OUTER JOIN [ChessDB-2].[dbo].[GameTotalHistoryByLength] (49) AS B2 ON B1.[History] = B2.[History] AND B1.[NextMove] = B2.[NextMove]
+) AS BOOKJOIN
+WHERE Total > 8
+
+select History,NextMove, SUM(Total) AS TotalGames
+from
+	(
+		select * from [ChessDB-1].[dbo].[GameTotalHistoryByLength](49)
+		union all
+		select * from [ChessDB-2].[dbo].[GameTotalHistoryByLength](49)
+	) AS UNIONALL
+Group by History,NextMove
+having SUM(Total) > 8

1308ada4dbbf06c1776fe19de46e99abfb6d1155
Daniel Baklan <danielbaklan16@gmail.com>
2025-07-28 07:09:20 +0300

    Split evaluation by piece

---
+++ KioChess/Engine/Models/Boards/Board.Bishop.Evaluation.cs
@@ -0,0 +1,108 @@
+﻿using Engine.Models.Enums;
+using Engine.Models.Helpers;
+using System.Runtime.CompilerServices;
+
+namespace Engine.Models.Boards
+{
+    public partial class Board
+    {
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int GetWhiteBishopValue()
+        {
+            var bits = _boards[Pieces.WhiteBishop];
+            int value = bits.Count() > 1 ? _evaluationService.GetDoubleBishopValue() : 0;
+
+            while (bits.Any())
+            {
+                var coordinate = bits.BitScanForward();
+                value += _evaluationService.GetWhiteBishopFullValue(coordinate);
+
+                value += GetWhiteBishopPinsOpening(coordinate);
+
+                //if ((_whiteMinorDefense[coordinate] & _boards[Pieces.WhitePawn]).Any())
+                //{
+                //    value += _evaluationService.GetMinorDefendedByPawnValue();
+                //}
+
+                value += GetWhiteBishopMobility(coordinate);
+                bits = bits.Remove(coordinate);
+            }
+
+            return value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int GetBlackBishopValue()
+        {
+            var bits = _boards[Pieces.BlackBishop];
+            int value = bits.Count() > 1 ? _evaluationService.GetDoubleBishopValue() : 0;
+            while (bits.Any())
+            {
+                var coordinate = bits.BitScanForward();
+                value += _evaluationService.GetBlackBishopFullValue(coordinate);
+
+                value += GetBlackBishopPinsOpening(coordinate);
+
+                //if ((_blackMinorDefense[coordinate] & _boards[Pieces.BlackPawn]).Any())
+                //{
+                //    value += _evaluationService.GetMinorDefendedByPawnValue();
+                //}
+
+                value += GetBlackBishopMobility(coordinate);
+                bits = bits.Remove(coordinate);
+            }
+
+            return value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int EvaluateWhiteBishopEnd()
+        {
+            var bits = _boards[Pieces.WhiteBishop];
+            int value = bits.Count() > 1 ? _evaluationService.GetDoubleBishopValue() : 0;
+
+            while (bits.Any())
+            {
+                var coordinate = bits.BitScanForward();
+                value += _evaluationService.GetWhiteBishopFullValue(coordinate);
+
+                value += GetWhiteBishopPinsEnd(coordinate);
+
+                //if ((_whiteMinorDefense[coordinate] & _boards[Pieces.WhitePawn]).Any())
+                //{
+                //    value += _evaluationService.GetMinorDefendedByPawnValue();
+                //}
+
+                value += GetWhiteBishopMobility(coordinate);
+                bits = bits.Remove(coordinate);
+            }
+
+            return value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int EvaluateBlackBishopEnd()
+        {
+            var bits = _boards[Pieces.BlackBishop];
+            int value = bits.Count() > 1 ? _evaluationService.GetDoubleBishopValue() : 0;
+            while (bits.Any())
+            {
+                var coordinate = bits.BitScanForward();
+                value += _evaluationService.GetBlackBishopFullValue(coordinate);
+
+                value += GetBlackBishopPinsEnd(coordinate);
+
+                //if ((_blackMinorDefense[coordinate] & _boards[Pieces.BlackPawn]).Any())
+                //{
+                //    value += _evaluationService.GetMinorDefendedByPawnValue();
+                //}
+
+                value += GetBlackBishopMobility(coordinate);
+                bits = bits.Remove(coordinate);
+            }
+
+            return value;
+        }
+    }
+}
---
+++ KioChess/Engine/Models/Boards/Board.Knight.Evaluation.cs
@@ -0,0 +1,56 @@
+﻿using Engine.Models.Enums;
+using Engine.Models.Helpers;
+using System.Runtime.CompilerServices;
+
+namespace Engine.Models.Boards
+{
+    public partial class Board
+    {
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int GetWhiteKnightValue()
+        {
+            int value = 0;
+
+            var bits = _boards[Pieces.WhiteKnight];
+            while (bits.Any())
+            {
+                var coordinate = bits.BitScanForward();
+
+                value += _evaluationService.GetWhiteKnightFullValue(coordinate);
+                //if ((_whiteMinorDefense[coordinate] & _boards[Pieces.WhitePawn]).Any())
+                //{
+                //    value += _evaluationService.GetMinorDefendedByPawnValue();
+                //}
+
+                value += GetWhiteKnightMobility(coordinate);
+
+                bits = bits.Remove(coordinate);
+            }
+
+            return value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int GetBlackKnightValue()
+        {
+            int value = 0;
+            var bits = _boards[Pieces.BlackKnight];
+            while (bits.Any())
+            {
+                var coordinate = bits.BitScanForward();
+                value += _evaluationService.GetBlackKnightFullValue(coordinate);
+
+                //if ((_blackMinorDefense[coordinate] & _boards[Pieces.BlackPawn]).Any())
+                //{
+                //    value += _evaluationService.GetMinorDefendedByPawnValue();
+                //}
+
+                value += GetBlackKnightMobility(coordinate);
+                bits = bits.Remove(coordinate);
+            }
+
+            return value;
+        }
+    }
+}
